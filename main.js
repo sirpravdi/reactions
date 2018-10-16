25dde86858f14292fb41911ce6b81c05cec25d8b
sirpravdi <ollias17@gmail.com>
2018-10-16 16:31:51 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 (function (){
-  console.log('check');
-  document.body.setAttribute('style', 'display: grid; flex-flow: column');
+  var wrap = document.createElement('div');
+  wrap.className = 'wrapper';
+  document.body.append(wrap);
+  var title = document.createElement('input');
+  title.className = 'title';
+  wrap.append(title);
+  var moodbox = document.createElement('div');
+  moodbox.className = 'moodbox';
+  wrap.append(moodbox);
 })();
 
 
