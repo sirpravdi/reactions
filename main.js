2873026d975ec82cef8795329078d13ad0fc849c
sirpravdi <ollias17@gmail.com>
2018-10-16 17:41:41 +0300

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,13 @@
   var moodbox = document.createElement('div');
   moodbox.className = 'moodbox';
   wrap.append(moodbox);
+  
+  for(let i = 600; i<=638; i++){
+    let label = document.createElement(label);
+    label.setAttribute('data-emoji', String.fromCodePoint('0x1F' + i));
+    moodbox.append(label);
+  }
+  
 })();
 
 
