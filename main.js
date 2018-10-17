a97aa0b66869d2a89b88e1624526f95ffcdc1675
sirpravdi <ollias17@gmail.com>
2018-10-17 11:15:19 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 (function (){
-  var wrap = document.createElement('div');
+  let wrap = document.createElement('div');
   wrap.className = 'wrapper';
   document.body.append(wrap);
-  var title = document.createElement('input');
+  let title = document.createElement('input');
   title.className = 'title';
   title.placeholder = 'insert your text here...';
   wrap.append(title);
-  var moodbox = document.createElement('div');
+  let moodbox = document.createElement('div');
   moodbox.className = 'moodbox';
   wrap.append(moodbox);
   
@@ -29,6 +29,9 @@
     }
     label.append(input);
   }
+  let button = document.createElement('button');
+  button.className = 'moodbox_submit';
+  
   
 })();
 
