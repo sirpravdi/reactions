5f4ca109e3d0c66d2027db1ad1bf8ce35f9d321f
sirpravdi <ollias17@gmail.com>
2018-10-19 18:17:11 +0300

    Update main.js
    Create Reactions class

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-var Reactions = (function() {
+'use strict'
 
-  function createPoll(data) {
+class Reactions {
+
+   constructor(data) {
       let wrap = document.createElement('div');
       wrap.classList.add('reactions-wrapper');
       document.body.append(wrap);
@@ -20,7 +22,7 @@ var Reactions = (function() {
         let input = document.createElement('input');
         input.type = 'radio';
         input.name = 'poll';
-        input.addEventListener('click', pollClick);
+        input.addEventListener('click', Reactions.prototype.pollClick);
         label.append(input);
         let index = document.createElement('span');
         index.dataset.reactionsIndex = 'index' + i;
@@ -30,9 +32,9 @@ var Reactions = (function() {
         index.innerText = localStorage.getItem('index' + i);
         counter.append(index);
     })
-  };
+  }
 
-  function pollClick() {
+  pollClick() {
     var wrap = document.querySelector('.reactions-wrapper');
     if (wrap.querySelector('.reactions-wrapper__emoji_picked')) {
       var prev = wrap.querySelector('.reactions-wrapper__emoji_picked');
@@ -45,11 +47,8 @@ var Reactions = (function() {
       localStorage.setItem('index' + this.parentElement.dataset.reactionsCounter, parseInt(localStorage.getItem('index' + this.parentElement.dataset.reactionsCounter)) + 1);
       wrap.querySelector('[data-reactions-index="index' + this.parentElement.dataset.reactionsCounter + '"]').innerText = localStorage.getItem('index' + this.parentElement.dataset.reactionsCounter);
     }
-  };
-  
-  return{
-    create: createPoll
   }
-})();
+  
+};
 
-Reactions.create({title: 'What do yout think?', reactions: ['0x1F601','0x1F914','0x1F644']});
+new Reactions({title: 'What do yout think?', reactions: ['0x1F601','0x1F914','0x1F644']});
