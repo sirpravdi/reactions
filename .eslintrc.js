0348aa9ba55c28b1dc450d5dc85e38c1b97be5b3
sirpravdi <ollias17@gmail.com>
2018-10-22 23:02:23 +0300

    pass files from gitignore to rep

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": "codex",
+    "parserOptions": {
+        "ecmaVersion": 3
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
